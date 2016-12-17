e782bc0fb9364887332056ed777ec782672144d5
Gorka Esnal <gesnal@gmail.com>
2016-12-17 18:10:23 +0100

    Map reduce exercise

---
+++ mapreduce/src/main/java/es/deusto/bigdata/mapreduce/WordCountMapper.java
@@ -0,0 +1,26 @@
+package es.deusto.bigdata.mapreduce;
+
+import java.io.IOException; 
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+public class WordCountMapper extends org.apache.hadoop.mapreduce.Mapper<LongWritable, Text, Text, IntWritable>{
+	
+	private final static IntWritable one = new IntWritable(1);
+	private Text word = new Text();
+
+	public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter)
+			throws IOException {
+		String line = value.toString();
+		StringTokenizer tokenizer = new StringTokenizer(line);
+		while (tokenizer.hasMoreTokens()) {
+			word.set(tokenizer.nextToken());
+			output.collect(word, one);
+		}
+	}
+}
---
+++ mapreduce/src/main/java/es/deusto/bigdata/mapreduce/WordCountReducer.java
@@ -0,0 +1,21 @@
+package es.deusto.bigdata.mapreduce;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+	public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output,
+			Reporter reporter) throws IOException {
+		int sum = 0;
+		while (values.hasNext()) {
+			sum += values.next().get();
+		}
+		output.collect(key, new IntWritable(sum));
+	}
+}
