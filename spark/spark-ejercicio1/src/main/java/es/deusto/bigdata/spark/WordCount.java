2b485d5d13a13c3c02a5367d9ed7dca2b85d0bd7
Gorka Esnal <gesna@gmail.com>
2017-02-02 22:08:19 +0100

    Added spark exercise2

--- spark/spark-ejercicio1/src/main/java/es/deusto/bigdata/spark/WordCount.java
+++ spark/spark-ejercicio1/src/main/java/es/deusto/bigdata/spark/WordCount.java
@@ -13,28 +13,28 @@ import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 
 public class WordCount {
-	
+
 	@SuppressWarnings("serial")
 	public static void main(String[] args) throws Exception {
 		String inputFile = "C:\\tmp\\spark\\input.txt";
 		String outputFile = "C:\\tmp\\spark\\output";
-		
+
 		// Create a Java Spark Context.
 		SparkConf conf = new SparkConf().setAppName("wordCount").setMaster("local");
 		JavaSparkContext sc = new JavaSparkContext(conf);
-		
-		JavaRDD<String> input = sc.textFile(inputFile);
-		JavaRDD<String> words = input.flatMap(new FlatMapFunction<String, String>() {
-			public Iterable<String> call(String x) {
-				return Arrays.asList(x.split(" "));
+
+		JavaRDD<String> inputFileLines = sc.textFile(inputFile);
+		JavaRDD<String> words = inputFileLines.flatMap(new FlatMapFunction<String, String>() {
+			public Iterable<String> call(String inputFileLine) {
+				return Arrays.asList(inputFileLine.split(" "));
 			}
 		});
-		
+
 		// Transform into word and count.
 		JavaPairRDD<String, Integer> counts = words.mapToPair(new PairFunction<String, String, Integer>() {
 			@SuppressWarnings("unchecked")
-			public Tuple2<String, Integer> call(String x) {
-				return new Tuple2(x, 1);
+			public Tuple2<String, Integer> call(String word) {
+				return new Tuple2(word, 1);
 			}
 		}).reduceByKey(new Function2<Integer, Integer, Integer>() {
 			public Integer call(Integer x, Integer y) {
